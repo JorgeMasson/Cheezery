a7520c89ca8147e9652a3a9c8a9e374c8a618214
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-02-11 19:26:20 -0700

    Menu activity creada

--- app/src/main/java/masson/reynoso/cheezery/MainActivity.kt
+++ app/src/main/java/masson/reynoso/cheezery/MainActivity.kt
@@ -1,11 +1,21 @@
 package masson.reynoso.cheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn_start) as Button
+
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, RegisterActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/masson/reynoso/cheezery/RegisterActivity.kt
+++ app/src/main/java/masson/reynoso/cheezery/RegisterActivity.kt
@@ -1,11 +1,21 @@
 package masson.reynoso.cheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val button: Button = findViewById(R.id.btn_sign_in) as Button
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
