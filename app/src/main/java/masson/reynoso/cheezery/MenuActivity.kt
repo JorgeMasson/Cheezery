3d76c7e15ab8cca5027b79017ff2389cad0ced78
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-02-17 23:57:10 -0700

    Cold drinks

--- app/src/main/java/masson/reynoso/cheezery/MenuActivity.kt
+++ app/src/main/java/masson/reynoso/cheezery/MenuActivity.kt
@@ -1,11 +1,20 @@
 package masson.reynoso.cheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btn_colddrinks: Button = findViewById(R.id.btn_cold_drinks) as Button
+
+        btn_colddrinks.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/masson/reynoso/cheezery/Product.kt
@@ -0,0 +1,6 @@
+package masson.reynoso.cheezery
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
