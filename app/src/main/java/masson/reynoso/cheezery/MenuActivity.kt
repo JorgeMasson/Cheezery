874c3844dbb166a6416f3b7a4f74fde8bb2c77aa
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-02-20 18:03:48 -0700

    Pantallas de hot drinks, sweets y salties creadas

--- app/src/main/java/masson/reynoso/cheezery/MenuActivity.kt
+++ app/src/main/java/masson/reynoso/cheezery/MenuActivity.kt
@@ -11,9 +11,31 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btn_colddrinks: Button = findViewById(R.id.btn_cold_drinks) as Button
+        var btn_hotdrinks: Button = findViewById(R.id.btn_hot_drinks) as Button
+        var btn_sweets: Button = findViewById(R.id.btn_sweets) as Button
+        var btn_salties: Button = findViewById(R.id.btn_salties) as Button
 
         btn_colddrinks.setOnClickListener {
             var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "colddrinks")
+            startActivity(intent)
+        }
+
+        btn_hotdrinks.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "hotdrinks")
+            startActivity(intent)
+        }
+
+        btn_sweets.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "sweets")
+            startActivity(intent)
+        }
+
+        btn_salties.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "salties")
             startActivity(intent)
         }
     }
